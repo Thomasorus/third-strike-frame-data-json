5823f60cc8138c76fb80d43f62edce95c043256e
francisco <fmicaelli@accengage.com>
2017-08-18 16:10:06 +0200

    Turn int value into string

--- data/move.go
+++ data/move.go
@@ -1,3 +1,4 @@
+//Package data implements the structure of Json for Third Strike frame data.
 package data
 
 import (
@@ -10,18 +11,18 @@ import (
 type ThirdStrikeMove struct {
 	Name                 string
 	Common_Name          string
-	Startup              int
-	Hit                  int
-	Recovery             int
-	Block_advantage      int
-	Hit_advantage        int
-	Crouch_hit_advantage int
+	Startup              string
+	Hit                  string
+	Recovery             string
+	Block_advantage      string
+	Hit_advantage        string
+	Crouch_hit_advantage string
 	Cancel               Cancel
 	Parry                Parry
-	Kara_range           int
-	Throw_range          int
-	Damage               int
-	Stun                 int
+	Kara_range           string
+	Throw_range          string
+	Damage               string
+	Stun                 string
 	Bar_gain_attacker    BarGain
 	Bar_gain_opponent    BarGain
 }
@@ -41,9 +42,9 @@ type Parry struct {
 }
 
 type BarGain struct {
-	Whiff int
-	Hit   int
-	Block int
+	Whiff string
+	Hit   string
+	Block string
 }
 
 func (t ThirdStrikeMove) GetMove(characterData []byte, moveName string) (s string, err error) {
@@ -60,7 +61,7 @@ func (t ThirdStrikeMove) GetMove(characterData []byte, moveName string) (s strin
 			return
 		}
 	}
-	
+
 	return
 }
 
@@ -68,18 +69,18 @@ func (t ThirdStrikeMove) String() string {
 	var buffer bytes.Buffer
 	buffer.WriteString(fmt.Sprintf("Common_Name: %s\n", t.Common_Name))
 	buffer.WriteString(fmt.Sprintf("Name: %s\n", t.Name))
-	buffer.WriteString(fmt.Sprintf("Startup: %v\n", t.Startup))
-	buffer.WriteString(fmt.Sprintf("Hit: %v\n", t.Hit))
-	buffer.WriteString(fmt.Sprintf("Recovery: %v\n", t.Recovery))
-	buffer.WriteString(fmt.Sprintf("Block_advantage: %v\n", t.Block_advantage))
-	buffer.WriteString(fmt.Sprintf("Hit_advantage: %v\n", t.Hit_advantage))
-	buffer.WriteString(fmt.Sprintf("Crouch_hit_advantage: %v\n", t.Crouch_hit_advantage))
+	buffer.WriteString(fmt.Sprintf("Startup: %s\n", t.Startup))
+	buffer.WriteString(fmt.Sprintf("Hit: %s\n", t.Hit))
+	buffer.WriteString(fmt.Sprintf("Recovery: %s\n", t.Recovery))
+	buffer.WriteString(fmt.Sprintf("Block_advantage: %s\n", t.Block_advantage))
+	buffer.WriteString(fmt.Sprintf("Hit_advantage: %s\n", t.Hit_advantage))
+	buffer.WriteString(fmt.Sprintf("Crouch_hit_advantage: %s\n", t.Crouch_hit_advantage))
 	buffer.WriteString(fmt.Sprintf("Cancel: %s\n", t.Cancel.String()))
 	buffer.WriteString(fmt.Sprintf("Parry: %s\n", t.Parry.String()))
-	buffer.WriteString(fmt.Sprintf("Kara_range: %v\n", t.Kara_range))
-	buffer.WriteString(fmt.Sprintf("Throw_range: %v\n", t.Throw_range))
-	buffer.WriteString(fmt.Sprintf("Damage: %v\n", t.Damage))
-	buffer.WriteString(fmt.Sprintf("Stun: %v\n", t.Stun))
+	buffer.WriteString(fmt.Sprintf("Kara_range: %s\n", t.Kara_range))
+	buffer.WriteString(fmt.Sprintf("Throw_range: %s\n", t.Throw_range))
+	buffer.WriteString(fmt.Sprintf("Damage: %s\n", t.Damage))
+	buffer.WriteString(fmt.Sprintf("Stun: %s\n", t.Stun))
 	buffer.WriteString(fmt.Sprintf("Bar_gain_attacker: %s\n", t.Bar_gain_attacker.String()))
 	buffer.WriteString(fmt.Sprintf("Bar_gain_opponent: %s\n", t.Bar_gain_opponent.String()))
 	return buffer.String()
@@ -121,8 +122,10 @@ func (p Parry) String() string {
 
 func (b BarGain) String() string {
 	var s []string
-	s = append(s, fmt.Sprintf("Whiff: %v", b.Whiff))
-	s = append(s, fmt.Sprintf("Hit: %v", b.Hit))
-	s = append(s, fmt.Sprintf("Block: %v", b.Block))
+	if len(b.Whiff) != 0 {
+		s = append(s, fmt.Sprintf("Whiff: %s", b.Whiff))
+	}
+	s = append(s, fmt.Sprintf("Hit: %s", b.Hit))
+	s = append(s, fmt.Sprintf("Block: %s", b.Block))
 	return strings.Join(s, "/")
 }
